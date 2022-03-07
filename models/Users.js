2d25a0522a236649c328ffec3c19b9eab7b6458b
kelvin obiri <92574631+obirikan@users.noreply.github.com>
2022-03-07 19:38:19 +0000

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,6 @@
 // const mongoose = require("mongoose");
 const mongoose=require('mongoose')
-
+//everything could be in a different schema
 const userSchema = mongoose.Schema(
   {
     name: { type: "String", required: true },
